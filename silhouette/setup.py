d831e3342d434ee85c9bc4728b4e5ec82832a56e
Robert Brewer <fumanchu@aminus.org>
2013-11-11 12:02:08 -0800

    Buglet in callpoint exit

--- silhouette/setup.py
+++ silhouette/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='silhouette',
-    version='0.1.0',
+    version='0.1.1',
     author="Robert Brewer, JJ Del Carpio, Charles Waldman",
     author_email="dev@crunch.io",
     packages=find_packages('.'),
