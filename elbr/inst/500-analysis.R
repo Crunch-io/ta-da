ddb308d4dab0180a6a57d5b318d80a4d91fb05bf
Neal Richardson <neal@crunch.io>
2016-04-18 15:10:18 -0700

    Comment on grepping logs

--- elbr/inst/500-analysis.R
+++ elbr/inst/500-analysis.R
@@ -1,3 +1,6 @@
+## Made file with:
+## root@ahsoka:/var/www/logs/AWSLogs/910774676937/elasticloadbalancing/eu-west-1/2016/04# find . | xargs -n 1 egrep "\-1 \-1 \-1" > ../../../april-500s.log
+
 library(elbr)
 df <- cleanLog(read.elb("/var/www/logs/AWSLogs/910774676937/elasticloadbalancing/april-500s.log"))
 
