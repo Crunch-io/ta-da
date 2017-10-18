9004752271d3f5f5bb11a7909318bedf2ba21a79
Alessandro Molina <alessandro.molina@axant.it>
2017-10-18 15:59:30 +0200

    Grab only last 2000 modified datasets when picking one, whole list if a hundreds of MBs big JSON

--- datasetreplay/datasetreplay/datasetpick.py
+++ datasetreplay/datasetreplay/datasetpick.py
@@ -38,7 +38,8 @@ def main():
 
     hosts = ENVIRONS[env]
     with tunnel(hosts[0], 8081, 29081, hosts[1]) as connection:
-        resp = requests.get(**admin_url(connection, '/datasets/?show_all=1&sorting=-modification_time'))
+        resp = requests.get(**admin_url(connection,
+                                        '/datasets/?show_all=1&sorting=-modification_time&limit=2000'))
         if resp.status_code != 200:
             print('ERROR: %s' % resp.text, file=sys.stderr)
             sys.exit(1)
