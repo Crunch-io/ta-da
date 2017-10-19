7b099ea930f0544438226abc2c03d43a6607308d
Alessandro Molina <alessandro.molina@axant.it>
2017-10-19 16:00:13 +0200

    Improve reporting

--- datasetreplay/datasetreplay/crtracefilereport.py
+++ datasetreplay/datasetreplay/crtracefilereport.py
@@ -3,21 +3,25 @@ from __future__ import print_function
 import json
 import sys
 import docopt
+import logging
 from . import slack
+
+log = logging.getLogger(__name__)
 
 USE_SLACK = False
 
 
-def notify(title, date, success, text):
+def notify(title, date, success, text, ratio):
+    title = '%s for %s (%s/%s)' % (title, date, ratio[0], ratio[1])
     if USE_SLACK:
         r = slack.message(channel="api", username="crunchbot",
                           icon_emoji=":grinning:" if success else ':worried:',
-                          attachments=[{'title': '%s for %s' % (title, date),
+                          attachments=[{'title': title,
                                         'text': '```%s```' % text,
                                         "mrkdwn_in": ["text"]}])
         r.raise_for_status()
     else:
-        print('Dataset Replay Report fo %s' % date)
+        print(title)
         print(text)
 
 
@@ -26,7 +30,7 @@ def main():
     helpstr = """Report a tracefile content from a specific day
 
     Usage:
-      %(script)s <tracefile> <date> <title> [--slack]
+      %(script)s <tracefile> <date> <title> [--slack] [--failures]
       %(script)s (-h | --help)
 
     Arguments:
@@ -44,18 +48,30 @@ def main():
     date = arguments['<date>']
     title = arguments['<title>']
     USE_SLACK = arguments['--slack']
+    failures_only = arguments['--failures']
 
-    failures = False
+    total = 0
+    failures = 0
     loglines = []
     with open(tracefile, 'r') as f:
         for l in f:
-            logline = json.loads(l)
+            try:
+                logline = json.loads(l)
+            except ValueError:
+                log.exception('Invalid line: %s', l)
+                # Spurious line???
+                pass
+
             if date == logline['date']:
+                total += 1
                 if not logline['success']:
-                    failures = True
+                    failures += 1
+                if failures_only and logline['success']:
+                    continue
                 loglines.append(logline['format'] % logline)
 
-    notify(title, date, not failures, '\n\n'.join(loglines))
+    notify(title, date, failures == 0, '\n'.join(loglines),
+           (total-failures, total))
 
 
 
