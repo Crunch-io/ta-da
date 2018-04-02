b85753d65fbcb637741561776bb001adb28487dd
David Handy <david@crunch.io>
2018-04-02 23:34:42 +0000

    david-find-fix-broken-ds:progress

--- bigds/bigds/ds_fix.py
+++ bigds/bigds/ds_fix.py
@@ -1,30 +1,28 @@
 """
 Helper script for finding and doing stuff with broken datasets
+
+Usage:
+    ds.fix dostuff
 """
 from __future__ import print_function
+import sys
+import time
+
+import docopt
+
+
+def do_stuff(args):
+    print("Here I am")
 
 
 def main():
     args = docopt.docopt(__doc__)
     t0 = time.time()
     try:
-        if args['get']:
-            return do_get(args)
-        elif args['info']:
-            return do_info(args)
-        elif args['anonymize']:
-            return do_anonymize(args)
-        elif args['post']:
-            return do_post(args)
-        elif args['addvar']:
-            return do_addvar(args)
-        elif args['folderize']:
-            return do_folderize(args)
-        elif args['loadsave']:
-            return do_loadsave(args)
-        else:
-            raise NotImplementedError(
-                "Sorry, that command is not yet implemented.")
+        if args["dostuff"]:
+            return do_stuff(args)
+        print("No command given.", file=sys.stderr)
+        return 1
     finally:
         print("Elapsed time:", time.time() - t0, "seconds", file=sys.stderr)
 
