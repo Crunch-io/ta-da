f76f73f814b48c18dea9d7845a7d82331f305390
David Handy <david@crunch.io>
2018-04-09 15:19:15 +0000

    ds.mongo-move: Fix hostname check, back to using dirs

--- bigds/bigds/ds_mongo_move.py
+++ bigds/bigds/ds_mongo_move.py
@@ -2,8 +2,11 @@
 Move documents related to a dataset from one Mongo cluster to another.
 
 Usage:
-    ds.mongo-move dump <ds-id> <archive-filename>
-    ds.mongo-move load <archive-filename>
+    ds.mongo-move [options] dump <ds-id> <output-dir>
+    ds.mongo-move [options] load <input-dir>
+
+Options:
+    --old-mongo    Mongo < 3.4.6, don't use "--uri" option to connect to Mongo.
 """
 from __future__ import print_function
 import os
@@ -12,6 +15,7 @@ import string
 import subprocess
 
 import docopt
+from six.moves.urllib.parse import urlparse
 import yaml
 
 # Mongo collections that this script will filter, dump, and load
@@ -25,7 +29,7 @@ COLLECTIONS = {
     'dataset_families': '{"crunch_id": "$ds_id"}',
     # 'projects',
     # 'project_dataset_order',
-    # 'dataset_order': {},
+    # 'dataset_order',
 }
 
 
@@ -35,44 +39,62 @@ def load_config():
         return yaml.safe_load(f)
 
 
+def _add_connection_params(cmd, args, config):
+    # Modify cmd, adding Mongo connection parameters based on args and config.
+    mongo_url = config['APP_STORE']['URL']
+    if args['--old-mongo']:
+        # For Mongo < 3.4.6, the --uri parameter is not available
+        db_name = u.path.lstrip('/')
+        connection_params = []
+        if u.hostname:
+            connection_params.extend(['--host', u.hostname])
+        if u.port:
+            connection_params.extend(['--port', str(u.port)])
+        if db_name:
+            connection_params.extend(['--db', db_name])
+        if u.username:
+            connection_params.extend(['--username', u.username])
+        if u.password:
+            connection_params.extend(['--password', u.password])
+        cmd[1:1] = connection_params
+    else:
+        cmd[1:1] = ['--uri', mongo_url]
+
+
 def do_dump(args):
     ds_id = args['<ds-id>']
+    output_dir = args['<output-dir>']
     archive_filename = args['<archive-filename>']
     config = load_config()
-    mongo_url = config['APP_STORE']['URL']
     for collection_name, query in COLLECTIONS.items():
         query_instance = string.Template(query).substitute(ds_id=ds_id)
         cmd = [
             'mongodump',
-            '--uri', mongo_url,
             '--collection', collection_name,
             '--query', query_instance,
-            '--archive={}'.format(archive_filename),
+            '--out', output_dir,
         ]
+        _add_connection_params(cmd, args, config)
         print(subprocess.list2cmdline(cmd))
         subprocess.check_call(cmd)
 
 
 def do_load(args):
-    XXX
-    # I need to parse the mongo hostname out of mongo_url
-    # and check against that hostname, not this one!
-    if 'mongo' in platform.node().lower():
-        raise RuntimeError(
-            "Hostname '{}' looks like a production mongo system. Aborting."
-            .format(platform.node()))
-    archive_filename = args['<archive-filename>']
+    input_dir = args['<input-dir>']
     config = load_config()
     mongo_url = config['APP_STORE']['URL']
-    if not args['--old-mongo']:
-        cmd = [
-            'mongorestore',
-            '--uri', mongo_url,
-            '--archive={}'.format(archive_filename),
-        ]
-    else:
-        # For Mongo < 3.4.6, the --uri parameter is not available
-        XXX
+    u = urlparse(mongo_url)
+    if u.hostname is not None and 'mongo' in u.hostname.lower():
+        raise RuntimeError(
+            "Hostname '{}' looks like a production mongo system. Aborting."
+            .format(u.hostname))
+    cmd = [
+        'mongorestore',
+        '--noIndexRestore',
+        '--noOptionsRestore',
+        input_dir,
+    ]
+    _add_connection_params(cmd, args, config)
     print(subprocess.list2cmdline(cmd))
     subprocess.check_call(cmd)
 
