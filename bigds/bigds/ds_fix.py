cbaaa0d8470389d780470a40cdfe15ae1bb159a6
David Handy <david@crunch.io>
2018-04-13 18:56:39 +0000

    ds.fix: Stub implementation of restore-from-s3

--- bigds/bigds/ds_fix.py
+++ bigds/bigds/ds_fix.py
@@ -4,13 +4,14 @@ Helper script for finding and doing stuff with broken datasets
 Usage:
     ds.fix [options] look <ds-id> [<ds-version>]
     ds.fix [options] restore-from-savepoint <ds-id> [<ds-version>]
+    ds.fix [options] restore-from-s3 <ds-id>
 
 Options:
     -i                      Run interactive prompt after the command
     --cr-lib-config=FILENAME
                             [default: /var/lib/crunch.io/cr.server-0.conf]
 
-WARNING!!! restore-from-savepoint is currently experimental/dangerous.
+WARNING!!! The restore-* commands are currently experimental/dangerous.
 """
 from __future__ import print_function
 import sys
@@ -64,14 +65,34 @@ def do_restore_from_savepoint(args, ds_id, ds_version=None):
     }
 
 
+def do_restore_from_s3(args, ds_id):
+    """
+    Steps to restore a dataset from S3:
+    1) See if the dataset is leased.  If so, release it.
+    2) Go to zz9data and remove the data directory.
+        - Make double-dog sure you are in zz9data
+        - make sure it's gone from both zz9 servers.
+    3) Go to zz9repo and rename the dataset directory in the repo to <dsid>-aside.
+    4) Try to access the dataset in superadmin. This should pull it from s3 and
+       migrate it.
+    5) Check the version in the repo.
+        - do a ds.release() to push and release the dataset
+        - use lz4cat to check that the version in zz9repo is now 25
+    """
+    _cr_lib_init(args)
+    print("XXX work-in-progress", file=sys.stderr)
+    return 1
+
+
 def _do_command(args):
     t0 = time.time()
     try:
         if args['look']:
             return do_look(args, args['<ds-id>'], args['<ds-version>'])
         if args['restore-from-savepoint']:
-            return do_restore_from_savepoint(args, args['<ds-id>'],
-                                             args['<ds-version>'])
+            return do_restore_from_savepoint(args, args['<ds-id>']
+        if args['restore-from-s3']:
+            return do_restore_from_s3(args, args['<ds-id>'])
         print("No command, or command not implemented yet.", file=sys.stderr)
         return 1
     finally:
