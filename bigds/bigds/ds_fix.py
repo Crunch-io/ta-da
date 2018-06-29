7aeb356061c0911599232fd62d2678af626bf222
David Handy <david@crunch.io>
2018-06-28 22:54:11 -0400

    ds.fix: Got replay-from and unsafe-restore-tip commands working

--- bigds/bigds/ds_fix.py
+++ bigds/bigds/ds_fix.py
@@ -3,16 +3,18 @@ Helper script for finding and doing stuff with broken datasets
 
 Usage:
     ds.fix [options] list-versions <ds-id>
+    ds.fix [options] replay-from <ds-id> <ds-version>
+    ds.fix [options] unsafe-restore-tip <ds-id> <ds-version>
     ds.fix [options] list-all-actions [--long] <ds-id>
     ds.fix [options] save-actions <ds-id> <ds-version> <filename>
     ds.fix [options] show-actions [--long] <filename>
-    ds.fix [options] revert-to-version <ds-id> <ds-version>
     ds.fix [options] replay-actions <ds-id> <filename>
 
 Options:
     -i                      Run interactive prompt after the command
     --cr-lib-config=FILENAME
                             [default: /var/lib/crunch.io/cr.server-0.conf]
+    --owner-email=EMAIL     [default: captain@crunch.io]
 
 WARNING!!! these commands are currently experimental and/or dangerous.
 """
@@ -27,15 +29,14 @@ from six.moves import cPickle as pickle
 
 from cr.lib.commands.common import load_settings, startup
 from cr.lib import actions as actionslib
-from cr.lib.entities.actions import Action
 from cr.lib.entities.datasets.dataset import Dataset
 from cr.lib.entities.datasets.versions.versioning import (
     version_health,
     VersionTag,
 )
-import cr.lib.index.indexer
+from cr.lib.entities.users import User
 from cr.lib.settings import settings
-from zz9lib import frame as framelib
+from cr.lib import stores
 
 this_module = sys.modules[__name__]
 
@@ -54,12 +55,90 @@ def do_list_versions(args):
     _cr_lib_init(args)
     version_list = version_health(ds_id)
     print(len(version_list), "versions:")
-    for version_info in version_list:
-        pprint.pprint(version_info)
+    for version_id, version_info in version_list:
+        print("{:32} {}".format(version_id, version_info['date']))
     return {
         'ds_id': ds_id,
         'version_list': version_list,
     }
+
+
+def do_replay_from(args):
+    ds_id = args['<ds-id>']
+    ds_version = args['<ds-version>']
+    _cr_lib_init(args)
+    source_dataset = Dataset.find_one_with_timeout(
+        dict(id=ds_id, version='master__tip'), timeout=5, wait=0.5)
+    newds_id = stores.gen_id()
+    dataset_name = "REPLAY: " + source_dataset.name
+    dataset_owner = User.get_by_email(args['--owner-email'])
+    newds = Dataset(
+        id=newds_id,
+        name=dataset_name,
+        owner_type='User',
+        owner_id=dataset_owner.id,
+    )
+    newds.replay_from(source_dataset, from_version=ds_version, task=None)
+    print("Created new dataset by replaying dataset", ds_id, "from savepoint",
+          ds_version)
+    print("New dataset ID:", newds.id)
+    print("New dataset Name:", newds.name)
+
+
+def do_unsafe_restore_tip(args):
+    """
+    Remove the master__tip version of dataset <ds-id> and re-create it by
+    re-playing actions starting at savepoint <ds-version>.
+    No "safety-net" is provided if replaying fails - the master__tip version
+    will be lost or corrupted.
+    """
+    ds_id = args['<ds-id>'].strip()
+    ds_version = args['<ds-version>'].strip()
+    assert ds_id and ds_version
+    _cr_lib_init(args)
+    target_ds = Dataset.find_one_with_timeout(
+        dict(id=ds_id, version='master__tip'), timeout=5, wait=0.5)
+    print("About to call: _unsafe_restore_tip({!r}, {!r})"
+          .format(target_ds, ds_version))
+    _unsafe_restore_tip(target_ds, ds_version)
+    print("Done.")
+
+
+def _unsafe_restore_tip(target_ds, from_version):
+    """
+    target_ds: Dataset that will have it's tip re-created
+    from_version: 'master__<revision>' version to replay from
+    """
+    from_branch = Dataset.version_branch(from_version)
+    from_revision = Dataset.version_revision(from_version)
+    if from_branch != target_ds.branch:
+        raise ValueError(
+            'Start and End versions of replay must be on same branch.')
+    with actionslib.dataset_lock('unsafe_restore_tip', target_ds.id,
+                                 dataset_branch=target_ds.branch,
+                                 exclusive=True):
+        vtag = VersionTag.nearest(target_ds.id, from_branch, from_revision)
+        if vtag is None or vtag.revision(vtag.version) != from_revision:
+            raise ValueError('There is no savepoint at that revision')
+
+        # Grab delta from that savepoint up to <branch>__tip
+        savepoint_action = vtag.action
+        actions_to_replay = actionslib.for_dataset(
+            target_ds, None, since=savepoint_action, only_successful=True,
+            exclude_hashes=[savepoint_action.hash], replay_order=True
+        )
+
+        # Revert from <branch>__tip back to the savepoint.
+        # This also deletes the actions between the savepoint and tip.
+        target_ds.restore_savepoint(vtag)
+
+        # Replay those actions to get the dataset back the way it was.
+        target_ds.play_workflow(
+            None,
+            actions_to_replay,
+            autorollback=target_ds.AutorollbackType.Disabled,
+            task=None,
+        )
 
 
 def do_list_all_actions(args):
@@ -124,7 +203,7 @@ def do_show_actions(args):
     print(len(actions_list), "actions:")
     for action in actions_list:
         if args['--long']:
-            pprint.pprint(actions)
+            pprint.pprint(action)
         else:
             pprint.pprint(_abbreviate_action(action))
     return {
@@ -141,34 +220,6 @@ def _abbreviate_action(action):
     d['params.dataset.id'] = params['dataset']['id']
     d['params.dataset.branch'] = params['dataset']['branch']
     return d
-
-
-def do_revert_to_version(args):
-    ds_id = args['<ds-id>']
-    ds_version = args['<ds-version>']
-    _cr_lib_init(args)
-    svp = VersionTag.find_one(dict(dataset_id=ds_id, version=ds_version))
-    if not svp:
-        raise ValueError("Couldn't find savepoint for "
-                         "ds_id={} branch={} revision={}"
-                         .format(ds_id, ds.branch, ds.revision))
-    ds = Dataset.find_by_id(id=ds_id, version='master__tip')
-    ds.drop_versions(['master__tip'])
-    framelib.ZZ9Dataset.create(ds)
-    ds.restore_savepoint(svp)
-    # XXX
-    # I don't know if the release commands are necessary or helpful.
-    # I'm attempting to get the right format in the repository -
-    # Currently the format in the repo directories only gets up to 23, not 25.
-    ds.query(dict(command='release', push=True))
-    ds2 = Dataset.find_by_id(id=ds_id, version=ds_version)
-    ds2.query(dict(command='release', push=True))
-    return {
-        'ds_id': ds_id,
-        'ds_version': ds_version,
-        'ds': ds,
-        'svp': svp,
-    }
 
 
 def do_replay_actions(args):
