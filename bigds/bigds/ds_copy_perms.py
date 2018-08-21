9e85509a1a83417d54b2f5bc2c36586c1fc3a586
David Handy <david@crunch.io>
2018-08-21 16:53:50 -0400

    ds_copy_perms.py: work-in-progress

--- bigds/bigds/ds_copy_perms.py
+++ bigds/bigds/ds_copy_perms.py
@@ -3,13 +3,15 @@
 Copy ownership and permissions from one dataset to another.
 
 Usage:
-    ds_copy_perms.py [options] <source-ds-id> <destination-ds-id>
+    ds_copy_perms.py [options] ls <source-ds-id>
+    ds_copy_perms.py [options] cp <source-ds-id> <destination-ds-id>
 
 Options:
     --cr-lib-config=FILENAME  [default: /var/lib/crunch.io/cr.server-0.conf]
 """
 from __future__ import print_function
 import os
+import pprint
 import sys
 
 import docopt
@@ -70,6 +72,26 @@ def _build_query(ds, collection):
     return query
 
 
+def ds_list_perms(ds_id):
+    try:
+        ds = Dataset.find_by_id(id=ds_id, version='master__tip')
+    except exceptions.NotFound:
+        print("Source dataset not found:", ds_id, file=sys.stderr)
+        return 1
+    for storename in DATASET_COLLECTIONS_UNVERSIONED:
+        print("STORENAME:", storname)
+        store = getattr(stores.stores, storename)
+        query = _build_query(storename)
+        with allow_reading_unversioned(
+            Permission, UserDatasetPreferences, UserVariableOrder, DeckOrder
+        ):
+            docs = store.find_all(query)
+            print(len(docs), "DOCS")
+            for doc in docs:
+                pprint.pprint(doc)
+    return 0
+
+
 def ds_copy_perms(source_ds_id, destination_ds_id):
     try:
         origin = Dataset.find_by_id(id=source_ds_id, version='master__tip')
@@ -93,13 +115,18 @@ def ds_copy_perms(source_ds_id, destination_ds_id):
 
 def main():
     args = docopt.docopt(__doc__)
-    source_ds_id = args['<source-ds-id>']
-    destination_ds_id = args['<destination-ds-id>']
     level = int(os.environ.get("CR_LOGLEVEL", "30"))
     loggers.StdoutLogger(bus, format="%(message)s\n", level=level).subscribe()
     log_to_stdout(level=30)
     _cr_lib_init(args)
-    return ds_copy_perms(source_ds_id, destination_ds_id)
+    source_ds_id = args['<source-ds-id>']
+    if args['ls']:
+        return ds_list_perms(source_ds_id)
+    if args['cp']:
+        destination_ds_id = args['<destination-ds-id>']
+        return ds_copy_perms(source_ds_id, destination_ds_id)
+    print("Unknown command.", file=sys.stderr)
+    return 1
 
 
 if __name__ == '__main__':
