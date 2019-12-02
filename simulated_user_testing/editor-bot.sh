dd2b9f42cd16a9a3bb6ba8832ca340be2a487682
David Handy <david@crunch.io>
2019-12-02 11:35:00 -0500

    editor-bot.sh: Change allow log files to be read by user centos

--- simulated_user_testing/editor-bot.sh
+++ simulated_user_testing/editor-bot.sh
@@ -4,7 +4,7 @@ export PYTHONUNBUFFERED=y
 
 TESTING_HOME=/remote/simulated_user_testing
 cd $TESTING_HOME
-umask 007
+umask 005
 
 # don't run if we're in maintmode (any of the different types)
 if [ -f /var/lib/crunch.io/maintenance -o -f /var/lib/crunch.io/maintenance-streaming -o -f /var/lib/crunch.io/maintenance-resetdata ]; then
