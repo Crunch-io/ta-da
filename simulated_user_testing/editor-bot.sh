4de335d2d19e0cc5aba476ca0224639e101b361f
David H <david@crunch.io>
2020-05-28 13:52:02 -0400

    Fix umask

--- simulated_user_testing/editor-bot.sh
+++ simulated_user_testing/editor-bot.sh
@@ -4,7 +4,7 @@ export PYTHONUNBUFFERED=y
 
 TESTING_HOME=/remote/simulated_user_testing
 cd $TESTING_HOME
-umask 005
+umask 002
 
 # don't run if we're in maintmode (any of the different types)
 if [ -f /var/lib/crunch.io/maintenance -o -f /var/lib/crunch.io/maintenance-streaming -o -f /var/lib/crunch.io/maintenance-resetdata ]; then
