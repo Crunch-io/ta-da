f63d5a8bb20d39cd244699bcfd0495dc5711572b
Alessandro Molina <amol@turbogears.org>
2020-10-23 16:29:24 +0200

    First import

---
+++ adminreport/report-integrity-alpha.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -e
+adminreport /dataset_integrity broken --env=alpha -k dataset_id -k status --top=10
---
+++ adminreport/report-integrity-prod.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -e
+adminreport /dataset_integrity broken --env=eu -k dataset_id -k status --top=10
