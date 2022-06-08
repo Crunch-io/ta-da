9b749f584a0c0326ee031fde64a3b576ac8b9ce6
Crunchbot <systems+crunchbot@crunch.io>
2022-06-08 14:50:47 +0000

    Updating built site (build 703)

--- js/script.js
+++ js/script.js
@@ -169,7 +169,6 @@ $(document).ready(function() {
     $('.video-btn').click(function() {
         $videoSrc = $(this).data( "src" );
     });
-    console.log($videoSrc);
 
     $('#myModal').on('shown.bs.modal', function (e) {
         $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" );
@@ -178,6 +177,15 @@ $(document).ready(function() {
     $('#myModal').on('hide.bs.modal', function (e) {
         $("#video").attr('src',$videoSrc);
     })
+
+    // Hidden elements of accordion that ar active
+    var accordionTargets;
+    $("#accordion-container button").on("click",function(e){
+      accordionTargets = $(this).data("target");
+      $(accordionTargets).on('show.bs.collapse', function (e) {
+        jQuery(".hide.collapse.show").collapse('hide');
+      })
+    });
 
 // End
 });
